f44c555f81098666d6de5961eca74ea94bcf4093
sp4cew4rrior <jgcortezmarcelino@gmail.com>
2022-01-11 23:22:21 -0300

    Add files via upload

---
+++ dissecting a variable.py
@@ -0,0 +1,13 @@
+a = input('Please type something: ')
+print('Here are some information about what you typed. ')
+print('It primitive type is', type(a))
+print('Its just spaces?', a.isspace())
+print('It is alphanumeric?', a.isalnum())
+print('It is alphabetic?', a.isalpha())
+print('It is numeric?', a.isnumeric())
+print('It is in capital letters?', a.isupper())
+print('Is it in lower case?', a.islower())
+print('Is it decimal?', a.isdecimal())
+print('Is everything digits?', a.isdigit())
+
+
